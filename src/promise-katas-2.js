9160cb393056e712efe50b72e414ec1aac9a7f4d
Greg Cain <codegregcode@gmail.com>
2023-02-13 21:18:44 +0000

    completes promise katas 2

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,33 +25,51 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = async () => {
+  const response = await fetch("food");
+  return response.data;
+};
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = async () => {
+  const response = await fetch("cats");
+  return response.data.cats.sort();
+};
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = async () => {
+  const response = await fetch("dogs");
+
+  const badDog = response.data.dogs.reduce((prev, curr) =>
+    prev.naughty > curr.naughty ? prev : curr
+  );
+  return badDog;
+};
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
-// a joke object with the key of question and answer - expected return { 
+// a joke object with the key of question and answer - expected return {
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
 // } of type Object
-// You will have to make more than one request to our fakeApi to get all the data you need. 
+// You will have to make more than one request to our fakeApi to get all the data you need.
 // Be aware of nesting your calls (try to avoid callback hell). Take a look at Promise.all on MDN
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = async () => {
+  const question = await fetch("jokes", "question");
+  const joke = await fetch("jokes");
+
+  return { question: question.joke, answer: joke.answer };
+};
 
 module.exports = {
-    food,
-    cat,
-    dog,
-    joke
-}
+  food,
+  cat,
+  dog,
+  joke,
+};
